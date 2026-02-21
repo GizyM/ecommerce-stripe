c5848c95afa5e20b383a9e75eb83ab60310036e0
Gizy Michel <batblover@yahoo.com>
2026-02-21 18:18:44 -0500

    initialized git repository

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Carousel } from "@/components/carousel";
+import { Carousel } from "../components/carousel";
 
 export default async function Home() {
   const products = await stripe.products.list({
@@ -13,13 +12,25 @@ export default async function Home() {
 
   return (
    <div>
-    <section>
-      <div>
-        <div>
-          <h2>Welcome to My Ecommerce</h2>
-          <p>Discover the latest products at the best prices.</p>
-          <Button asChild variant="default">
-            <Link href="/products">Browse All Products</Link>
+    <section className="rounded bg-neutral-100 py-8 sm:py-12">
+      <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
+        <div className="max-w-md space-y-4">
+          <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
+            Welcome to My Ecommerce
+            </h2>
+          <p className="text-neutral-600">
+            Discover the latest products at the best prices.
+            </p>
+          <Button 
+          asChild 
+          variant="default"
+          className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white"
+          >
+            <Link href="/products"
+            className="inline-flex items-center justify-center rounded-full px-6 py-3"
+            >
+              Browse All Products
+              </Link>
           </Button>
         </div>
         <Image alt="Banner Image" 
@@ -28,9 +39,16 @@ export default async function Home() {
         src={products.data[0].images[0]} 
         />
       </div>
+      <Image
+      alt="Hero Image"
+      src={products.data[0].images[0]}
+      className="rounded"
+      width={450}
+      height={450}
+    />
     </section>
-    <section>
-      <Carousel />
+    <section className="py-8">
+      <Carousel products={products.data} />
     </section>
    </div>
   );
